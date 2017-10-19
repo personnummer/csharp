931a1d5380770d958b61b4c431abe7b2d67a3819
Johannes Tegnér <johannes@jitesoft.com>
2017-10-19 14:22:25 +0200

    Implemented the Personnumer class and updated tests to use theory instead of fact.

--- Personnummer.Tests/PersonnumerTests.cs
+++ Personnummer.Tests/PersonnumerTests.cs
@@ -1,86 +1,62 @@
-using System;
+using System.Diagnostics;
 using Xunit;
-using System.Collections.Generic;
-using Xunit.Extensions;
+using Xunit.Abstractions;
 
 namespace Personnummer.Tests
 {
     public class PersonnumerTests
     {
+
         [Theory]
-        [InlineData("510818-9167")]
-        [InlineData("19900101-0017")]
-        [InlineData("19130401+2931")]
-        [InlineData("196408233234")]
-        [InlineData("0001010107")]
-        [InlineData("000101-0107")]
-        public void TestPersonnummerWithControlDigitWithString(string value)
+        [InlineData("510818-9167", true)]
+        [InlineData("19900101-0017", true)]
+        [InlineData("19130401+2931", true)]
+        [InlineData("196408233234", true)]
+        [InlineData("0001010107", true)]
+        [InlineData("000101-0107", true)]
+        [InlineData("640327-381", false)]
+        [InlineData("510818-916", false)]
+        [InlineData("19900101-001", false)]
+        [InlineData("100101+001", false)]
+        public void TestPersonnummerString(string value, bool expected)
         {
-            Assert.True(Personnummer.Valid(value));
+            Assert.Equal(Personnummer.Valid(value), expected);
         }
 
 
         [Theory]
-        [InlineData(6403273813)]
-        [InlineData(5108189167)]
-        [InlineData(0001010107)]
-        [InlineData(199001010007)]
-        public void TestPersonnummerWithControlDigitWithInt(int value)
+        [InlineData(6403273813, true)]
+        [InlineData(5108189167, true)]
+        [InlineData(199001010017, true)]
+        [InlineData(640327381, false)]
+        [InlineData(510818916, false)]
+        [InlineData(19900101001, false)]
+        [InlineData(100101001, false)]
+        public void TestPersonnummerInt(long value, bool expected)
         {
-            Assert.True(Personnummer.Valid(value));
+            
+            Assert.Equal(Personnummer.Valid(value), expected);
+        }
+        
+        [Theory]
+        [InlineData("701063-2391", true)]
+        [InlineData("640883-3231", true)]
+        [InlineData("900161-0017", false)]
+        [InlineData("640893-3231", false)]
+        public void TexstCoOrdinationNumbersString(string value, bool expected)
+        {
+            Assert.Equal(Personnummer.Valid(value), expected);
         }
 
         [Theory]
-        [InlineData("640327-381")]
-        [InlineData("510818-916")]
-        [InlineData("19900101-001")]
-        [InlineData("100101+001")]
-        public void TestPersonnummerWithoutControlDigitString(string value)
+        [InlineData(7010632391, true)]
+        [InlineData(6408833231, true)]
+        [InlineData(9001610017, false)]
+        [InlineData(6408933231, false)]
+        public void TexstCoOrdinationNumbersInt(long value, bool expected)
         {
-            Assert.False(Personnummer.Valid(value));
+            Assert.Equal(Personnummer.Valid(value), expected);
         }
 
-
-        [Theory]
-        [InlineData(640327381)]
-        [InlineData(510818916)]
-        [InlineData(19900101001)]
-        [InlineData(100101001)]
-        public void TestPersonnummerWithoutControlDigitInt(int value)
-        {
-            Assert.False(Personnummer.Valid(value));
-        }
-
-        [Theory]
-        [InlineData("701063-2391")]
-        [InlineData("640883-3231")]
-        public void TexstCoOrdinationNumbersString(string value)
-        {
-            Assert.True(Personnummer.Valid(value));
-        }
-
-        [Theory]
-        [InlineData(7010632391)]
-        [InlineData(6408833231)]
-        public void TexstCoOrdinationNumbersInt(int value)
-        {
-            Assert.True(Personnummer.Valid(value));
-        }
-
-        [Theory]
-        [InlineData("900161-0017")]
-        [InlineData("640893-3231")]
-        public void TestWrongCoOrdinationNumbersString(string value)
-        {
-            Assert.False(Personnummer.Valid(value));
-        }
-
-        [Theory]
-        [InlineData(9001610017)]
-        [InlineData(6408933231)]
-        public void TestWrongCoOrdinationNumbersInt(int value)
-        {
-            Assert.False(Personnummer.Valid(value));
-        }
     }
 }
--- Personnummer/Personnummer.cs
+++ Personnummer/Personnummer.cs
@@ -1,18 +1,75 @@
-﻿namespace Personnummer
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Personnummer
 {
     /// <summary>
     /// Class used to verify Swedish social security numbers.
     /// </summary>
     public static class Personnummer
     {
-        private static int Luhn(string value)
+        private static readonly Regex regex;
+        private static readonly CultureInfo cultureInfo;
+
+        static Personnummer()
         {
-            return 0;
+            cultureInfo = new CultureInfo("sv-SE");
+            regex       = new Regex(@"(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([-|+]{0,1})?(\d{3})(\d{0,1})");
         }
 
-        private static bool TestDate()
+        /// <summary>
+        /// Calculates the checksum value of a given digit-sequence as string by using the luhn/mod10 algorithm.
+        /// </summary>
+        /// <param name="value">Sequense of digits as a string.</param>
+        /// <returns>Resulting checksum value.</returns>
+        private static int Luhn(string value)
         {
-            return false;
+            // Luhm algorithm doubles every other number in the value.
+            // To get the correct checksum digit we aught to append a 0 on the sequence.
+            // If the result becomes a two digit number, subtract 9 from the value.
+            // If the total sum is not a 0, the last checksum value should be subtracted from 10.
+            // The resulting value is the check value that we use as control number.
+            
+            char[] t = (value + "0").ToCharArray();
+            int sum = 0;
+            int temp;
+            // The checksum should be checked backwards.
+            for (int i = t.Length; i -->0; )
+            {
+                // The value should not be used as an int right away
+                // (because it is a char which is a number. We need to subtract the '0' value (48) from it too.
+                temp = t[i] - 48;
+                // Depending on i%2 (if even or odd) we either add or double.
+                sum += (i % 2 == 0) ? ((temp *= 2) > 9 ? temp - 9 : temp) : temp;
+            }
+
+            if (sum != 0)
+            {
+                sum = 10 - (sum % 10);
+            }
+            
+            return sum;
+        }
+
+        /// <summary>
+        /// Function to make sure that the passed year, month and day is parseable to a date.
+        /// </summary>
+        /// <param name="year">Years as string.</param>
+        /// <param name="month">Month as int.</param>
+        /// <param name="day">Day as int.</param>
+        /// <returns>Result.</returns>
+        private static bool TestDate(string year, int month, int day)
+        {
+            try
+            {
+                DateTime dt = new DateTime(cultureInfo.Calendar.ToFourDigitYear(int.Parse(year)), month, day);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         /// <summary>
@@ -22,17 +79,41 @@
         /// <returns>Result.</returns>
         public static bool Valid(string value)
         {
-            return false;
+            MatchCollection matches = regex.Matches(value);
+
+            if (matches.Count < 1 || matches[0].Groups.Count < 7)
+            {
+                return false;
+            }
+
+            GroupCollection groups = matches[0].Groups;
+            int month, day, check;
+            string yStr;
+            try
+            {
+                yStr  = (groups[2].Value.Length == 4) ? groups[2].Value.Substring(2) : groups[2].Value;
+                month = int.Parse(groups[3].Value);
+                day   = int.Parse(groups[4].Value);
+                check = int.Parse(groups[7].Value);
+            }
+            catch
+            {
+                // Could not parse. So invalid.
+                return false;
+            }
+
+            bool valid = Luhn(yStr + groups[3].Value + groups[4].Value + groups[6].Value) == check;
+            return valid && (TestDate(yStr, month, day) || TestDate(yStr, month, day - 60));
         }
 
         /// <summary>
         /// Validate Swedish social security number.
         /// </summary>
-        /// <param name="value">Value as integer.</param>
+        /// <param name="value">Value as long.</param>
         /// <returns>Result.</returns>
-        public static bool Valid(int value)
+        public static bool Valid(long value)
         {
-            return false;
+            return Valid(value.ToString());
         }
 
     }
