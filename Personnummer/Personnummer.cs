08e3ddf054f4ba42060883ff42d6adc8f5e3d6d5
Johannes Tegnér <johannes@jitesoft.com>
2017-10-19 09:13:02 +0200

    Created initial files. Added Tests for the Personnummer class. Created class for Personnumer (not yet implemented).

---
+++ Personnummer/Personnummer.cs
@@ -0,0 +1,39 @@
+﻿namespace Personnummer
+{
+    /// <summary>
+    /// Class used to verify Swedish social security numbers.
+    /// </summary>
+    public static class Personnummer
+    {
+        private static int Luhn(string value)
+        {
+            return 0;
+        }
+
+        private static bool TestDate()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Validate Swedish social security number.
+        /// </summary>
+        /// <param name="value">Value as string.</param>
+        /// <returns>Result.</returns>
+        public static bool Valid(string value)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Validate Swedish social security number.
+        /// </summary>
+        /// <param name="value">Value as integer.</param>
+        /// <returns>Result.</returns>
+        public static bool Valid(int value)
+        {
+            return false;
+        }
+
+    }
+}
